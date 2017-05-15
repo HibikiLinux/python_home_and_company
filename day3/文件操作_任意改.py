e4a4a520e9f3b5c0aebf01067267bcea9e64cc1b
HibikiLinux <yanshenyx@outlook.com>
2017-05-15 10:28:05 +0800

    111

--- day3/文件操作_任意改.py
+++ day3/文件操作_任意改.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env Python
 #-*- coding:utf-8 -*-
 #Author:yanshen
+import sys
 f=open("yesterday.txt","r",encoding="utf8")
 f_new=open("yesterday_bak.txt","w",encoding="utf8")
-
+find=sys.argv[1]
+replace=sys.argv[2]
 for line in f:
-    if "有那么多肆意的快乐等我享受" in line :
-        line=line.replace("有那么多肆意的快乐等我享受","alex")
+    if find in line :
+        line=line.replace(find,replace)
     f_new.write(line)
 f.close()
 f_new.close()
-
